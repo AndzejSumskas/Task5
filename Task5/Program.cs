f58f3a873f8df6cb36c42dc2dbfd3a005a6521b3
Andzej Sumskas <andzejsumskas1@gmail.com>
2021-06-21 12:37:54 +0300

    task5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace Task5
 {
@@ -11,17 +15,40 @@ namespace Task5
     {
         static void Main(string[] args)
         {
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Andzej\DataBaseTest.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Andzej\DataBaseTest.mdf;Integrated Security=True;Connect Timeout=30");         
+            char select = ' ';
 
-            con.Open();
-
-            ReadAllPersons(con);
-            
-            con.Close();
+            while(select != '1' || select != '2')
+            {
+                con.Open();
+                Console.Clear();
+                Console.WriteLine("Select option:");
+                Console.WriteLine("1 - ReadAllPersons ");
+                Console.WriteLine("2 - WriteAllDataToJson");
+                select = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Clear();
+                switch (select)
+                {
+                    case '1':
+                        //Reads All Persons from local database(PERSONS)
+                        ReadAllPersons(con);
+                        break;
+                    case '2':
+                        //Write All Persons from local database(PERSONS) to json file
+                        WriteAllDataToJson(con);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong select.");
+                        break;
+                }
+                con.Close();
+            }        
         }
 
         public static void ReadAllPersons(SqlConnection con)
         {
+            Console.Clear();
             SqlCommand command;
             SqlDataReader dataReader;
             string sql = "";
@@ -34,6 +61,30 @@ namespace Task5
             {
                 Console.WriteLine($"{dataReader.GetValue(0)} {dataReader.GetValue(1)} {dataReader.GetValue(2)}");
             }
+            Console.WriteLine("Press key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void WriteAllDataToJson(SqlConnection con)
+        {
+            Console.Clear();
+            SqlCommand command;
+            SqlDataReader dataReader;
+            string sql = "";
+            List<string> data = new List<string>();
+
+            sql = "Select * from PERSONS";
+            command = new SqlCommand(sql, con);
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                data.Add($"{dataReader.GetValue(0)} {dataReader.GetValue(1)} {dataReader.GetValue(2)}");
+            }
+            string json = JsonConvert.SerializeObject(data.ToArray(), Formatting.Indented);
+            File.WriteAllText(@"C:\Users\Andzej\Desktop\IT Tasks\HomeWork\Task5\Task5\path.json", json);
+
+            Console.WriteLine("All files was writen to json file.");
             Console.WriteLine("Press key to continue...");
             Console.ReadKey();
         }
